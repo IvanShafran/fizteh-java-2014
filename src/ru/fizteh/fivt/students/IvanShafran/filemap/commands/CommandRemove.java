931fabbab489ae3d54e0dd515287880dfd3d1244
Ivan Shafran <vanobox07@mail.ru>
2014-10-04 18:22:33 +0400

    написал remove

---
+++ src/ru/fizteh/fivt/students/IvanShafran/filemap/commands/CommandRemove.java
@@ -0,0 +1,38 @@
+package ru.fizteh.fivt.students.IvanShafran.filemap.commands;
+
+import ru.fizteh.fivt.students.IvanShafran.filemap.DBFile;
+import ru.fizteh.fivt.students.IvanShafran.shell.AbstractShell;
+import ru.fizteh.fivt.students.IvanShafran.shell.commands.Command;
+
+import java.util.ArrayList;
+
+
+public class CommandRemove extends Command {
+    DBFile dataBaseFile;
+
+    @Override
+    public void checkArgs(ArrayList<String> args) throws Exception {
+        if (args.size() == 0) {
+            throw new Exception("missing key");
+        }
+    }
+
+    public void execute(ArrayList<String> args) throws Exception {
+        checkArgs(args);
+
+        String key = args.get(0);
+
+        if (dataBaseFile.getHashMap().containsKey(key)) {
+            AbstractShell.printInformation("remove");
+        }
+        else {
+            AbstractShell.printInformation("not found");
+        }
+
+        dataBaseFile.getHashMap().remove(key);
+    }
+
+    CommandRemove(DBFile file) {
+        dataBaseFile = file;
+    }
+}
